b099a8e8040cdfa4f0f70a5f79c3b92823f2c633
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2025-05-05 14:58:56 +0000

    Messing around with the window title

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -7,9 +7,6 @@ import MyButton from "./MyButton"
 import WindowTitle from "./WindowTitle"
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
-
-	// TODO: Make the title have a max length, this will make it nicer to have there
-
 	return <window
 		visible
 		cssClasses={["Bar"]}
@@ -19,6 +16,5 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
 			<WindowTitle />
 			<MyButton />
 		</box>
-
 	</window >
 }
--- widget/WindowTitle.tsx
+++ widget/WindowTitle.tsx
@@ -3,14 +3,20 @@ import Hyprland from "gi://AstalHyprland";
 
 
 export default function WindowTitle() {
+
 	const hypr = Hyprland.get_default()
 	const focused_client = bind(hypr, "focused_client")
+
+	// TODO: Make the title have a max length, this will prevent jank later on
 
 	return <box>{
 		focused_client.as(
 			client => (
 				client &&
-				<label label={bind(client, "title").as(String)} />)
+				<label label={
+					bind(client, "title")
+						.as(String)
+				} />)
 		)
 	}</box >
 }
